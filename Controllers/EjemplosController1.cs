57d79b02450686cb5cb0eccd6694c34ac09fae86
Victor Salgado <victor.vu583@gmail.com>
2023-10-09 15:38:20 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/EjemplosController1.cs
@@ -0,0 +1,25 @@
+﻿using _24CV_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _24CV_Web.Controllers
+{
+    public class EjemplosController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Victor";
+            persona.Apellidos = "Salgado";
+            persona.FechaNacimiento = new DateTime(2003, 02, 22);
+
+            return View(persona);
+        }
+
+
+    }
+}
